86f561244e7e3d1531c50f7cdd9cdf0ea444cac2
Ivan <ivanleiva6464@gmail.com>
2025-07-01 15:48:15 -0300

    TP_17_parte_dos

---
+++ TP17/script/script3.js
@@ -0,0 +1,30 @@
+console.log ("Ping script.js");
+
+// Tipos de datos
+
+let numeros = 17;
+let string = "texto en JS";
+let booleano = true; // false 
+let nulo = null; 
+let indefinido;
+let array = ["texto", 55, false, null, undefined, 58];
+let objeto = {
+    edad: 42,
+    nombre: "ivan",
+    estado: true,
+};
+console.log(numeros);
+console.log(typeof(numeros));
+console.log(typeof(string));
+console.log(typeof(booleano));
+console.log(typeof(nulo));
+console.log(typeof(indefinido));
+console.log(typeof(objeto));
+console.log(typeof(array));
+
+let numA = "20";
+numA = Number(numA);
+let numB = 30;
+console.log(numA + numB);
+
+
