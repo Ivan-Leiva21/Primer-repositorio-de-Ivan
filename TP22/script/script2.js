0887c2eed32f42c0d026ea45d0ae403637905b2d
Ivan <ivanleiva6464@gmail.com>
2025-08-05 16:20:01 -0300

    TP23

---
+++ TP22/script/script2.js
@@ -0,0 +1,23 @@
+let tieneMapa = false; 
+
+if (tieneMapa) {
+    console.log("podemos avanzar!!");    //esto se ejecuta solo si es verdadero
+} else {
+    console.log("no podemos avanzar :(")
+}
+
+let esDeNoche = false;
+
+if (esDeNoche) {
+    console.log("es hora de dormir");
+} else {
+    console.log("vamo a labura");
+}
+
+let energia = 90;
+
+if (energia > 50) {
+    console.log("avanzar sobre la montaña");
+} else {
+    console.log("tomar el camino del valle");
+}
---
+++ TP23/script/script1.js
@@ -0,0 +1,27 @@
+console.log(window);
+console.log(document);
+
+console.clear();
+
+console.log(document.querySelector("h1"));
+
+console.log(document.getElementsByTagName("p"));
+console.log(document.getElementsByClassName("parrafo"));
+console.log(document.getElementById("contenidoPrincipal"));
+
+console.log(document.querySelectorAll("p"));
+
+console.log(document.querySelectorAll(".parrafo"));
+
+console.log(document.querySelector("#contenidoPrincipal"));
+
+console.clear();
+
+console.log(document.querySelector("main h2").textContent);
+
+console.log(document.querySelector("main h2").textContent = "nuevo titulo desde js");
+
+console.log(document.querySelector("#contenidoPrincipal").textContent += " - modificado desde js");
+
+console.log(document.querySelector("main").innerHTML += '<p class="parrafo">parrafo 5</p>');
+
