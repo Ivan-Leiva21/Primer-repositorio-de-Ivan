5f2254fd6452b7f00af38563ca126c6ea840e587
Ivan <ivanleiva6464@gmail.com>
2025-08-14 16:16:55 -0300

    Validacion_repaso

--- TP23/script/script3.js
+++ TP23/script/script3.js
@@ -1,19 +1,70 @@
 document.querySelector("form").addEventListener("submit", (e) => {
     console.log("formulario enviado");
     e.preventDefault();
-    validarFormulario();
+    const resultado = validarFormulario();
+    // if(resultado){
+    //     console.log("formulario validado con exito");
+    // }else{
+    //     console.log("no se pudo validar la informacion");
+    // }
 });
-
-function validarFormulario(){
+function validarFormulario() {
     let resultado = true;
     let errores = [];
     const nombre = document.querySelector("#nombre");
+
+    // console.log(nombre.value);
+
     const apellido = document.querySelector("#apellido");
     const dni = document.querySelector("#dni");
 
-    if(nombre.value === "" || apellido.value === "" || dni.value === ""){
+    if (nombre.value === "" || apellido.value === "" || dni.value === "") {
+        resultado = false;
         console.log("debe completar todos los campos");
+
+        resultado = false;
+        errores.push("debe completar todos los campos");
+
+        if (isNaN(dni.value)) {
+            resultado = false;
+            errores.push("el campo DNI solo acepta numeros");
+        };
     }
-    resultado = false;
-    errores.push("debe completar todos los campos");
+    if (dni.value.length !== 8) {
+        resultado = false;
+        errores.push("el DNI debe tener 8 digitos");
+    };
+
+
+    
+    mostrarErrores(resultado, errores);
+};
+// console.log(errores);
+const letrasAprobadas = "abcdefghijklmnopqrstuvwxyzáéíóúäëïöü' ".split("");
+    console.log(letrasAprobadas);
+
+    const arrayNombre = nombre.value.split("");
+    arrayNombre.forEach((letra) => {
+        let query = letrasAprobadas.indexOf(letra.toLowerCase());
+        if (query === -1) {
+            resultado = false;
+            errores.push("el campo nombre solo acepta letras");
+        };
+    });
+
+
+function mostrarErrores(resultado, errores) {
+    if (resultado) {
+        //mostrar mensaje de exito
+        document.querySelector("#respuestas > div").innerHTML = "el formulario fue validado con exito";
+        document.querySelector("body").style.backgroundColor = "#666";
+
+    } else {
+        //mostrar mensaje de error y detalle de errores
+        document.querySelector("#respuestas > div").innerHTML = "el formulario no se pudo validar con exito";
+        document.querySelector("#respuestas > div").innerHTML += `<br> ${errores.join(" <br> ")}`;
+        document.querySelector("body").style.backgroundColor = "darkred";
+    }
+
+
 };
