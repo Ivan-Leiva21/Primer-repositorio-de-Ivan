d1f2e814c74f64cb362e259843dc01d6b0dd3094
Ivan <ivanleiva6464@gmail.com>
2025-10-09 15:48:51 -0300

    practica_tareas

--- TP28/Script/tareas.js
+++ TP28/Script/tareas.js
@@ -2,7 +2,6 @@ let tareas = [];
 
 document.querySelector("#btnAgregar").addEventListener("click", () => {
     cargarTarea();
-    document.querySelector("#tarea").value = "";
 });
 
 function cargarTarea() {
@@ -10,18 +9,18 @@ function cargarTarea() {
         window.alert("Ingrese su nombre de tarea para registrarla.");
         return;
     };
-};
 
-let tarea = {
-    id: tareas.length,
-    tarea: document.querySelector("#tarea").value,
-    estado: "pendiente"
-};
+    let tarea = {
+        id: tareas.length,
+        tarea: document.querySelector("#tarea").value,
+        estado: "pendiente"
+    };
 
-tareas.push(tarea);
-localStorage.setItem("tareas", JSON.stringify(tareas));
-document.querySelector("#tarea").value = "";
-mostrarDatos();
+    tareas.push(tarea);
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+    document.querySelector("#tarea").value = "";
+    mostrarDatos();
+};
 
 function mostrarDatos() {
     let tareasLocales = localStorage.getItem("tareas");
@@ -30,19 +29,14 @@ function mostrarDatos() {
     // console.log(tareasLocales);
     tareas = tareasLocales;
     // console.log(tareasLocales);
-};
+    document.querySelector("tbody").innerHTML = "";
 
-document.querySelector("tbody").innerHTML = "";
-
-tareas.forEach((tarea) => {
-    if(tarea.estado === "eliminada"){
-        return;
-    };
-});
-// console.log(tarea);
-
-document.querySelector("tbody").innerHTML +=
-`<tr>
+    tareasLocales.forEach((tarea) => {
+        if (tarea.estado === "eliminada") {
+            return;
+        };
+        document.querySelector("tbody").innerHTML +=
+            `<tr>
     <td>${tarea.id}</td>
     <td>${tarea.tarea}</td>
     <td>${estado()}</td>
@@ -51,67 +45,70 @@ document.querySelector("tbody").innerHTML +=
         <div class="btn btnBorrar" data-id="${tarea.id}">Borrar</div>
     </td>
 </tr>`;
+        function estado() {
+            if (tarea.estado === "pendiente") {
+                return `<div class="btn btnPendiente" data-id="${tarea.id}">Pendiente</div>`;
+            } else {
+                return `<div class="btn btnCompleta" data-id="${tarea.id}">Completa</div>`;
+            };
+        };
+    });
 
-function estado(){
-    if(tarea.estado === "pendiente"){
-        return `<div class="btn bntPendiente" data-id="${tarea.id}"Pendiente</div>`;
-    } else {
-        return `<div class="btn btnCompleta" data-id="${tarea.id}">Completa</div>`;
-    };
+    // Event Listeners
+    document.querySelectorAll(".btnPendiente").forEach((boton) => {
+        boton.addEventListener("click", (e) => {
+            // console.log("boton pendiente");
+            // console.log(e.target.dataset.id);
+            // console.log(tareas[e.target.dataset.id]);
+            tareas[e.target.dataset.id].estado = "completa";
+            localStorage.setItem("tareas", JSON.stringify(tareas));
+            mostrarDatos();
+        });
+    });
+
+    document.querySelectorAll(".btnCompleta").forEach((boton) => {
+        boton.addEventListener("click", (e) => {
+            tareas[e.target.dataset.id].estado = "pendiente";
+            localStorage.setItem("tareas", JSON.stringify(tareas));
+            mostrarDatos();
+        });
+    });
+
+    document.querySelectorAll(".btnBorrar").forEach((boton) => {
+        boton.addEventListener("click", (e) => {
+            tareas[e.target.dataset.id].estado = "eliminada";
+            localStorage.setItem("tareas", JSON.stringify(tareas));
+            mostrarDatos();
+            tareas.pop();
+        });
+    });
+
+    document.querySelectorAll(".btnEditar").forEach((boton) => {
+        boton.addEventListener("click", (e) => {
+            modificarTarea(e.target.dataset.id);
+        });
+    });
 };
 
-// Event Listeners
-document.querySelectorAll(".btnPendiente").forEach((boton) => {
-    boton.addEventListener("click", (e) => {
-        console.log("boton pendiente");
-        console.log(e.target.dataset.id);
-        console.log(tareas[e.target.dataset.id]);
-        tareas[e.target.dataset.id].estado = "completa";
-        localStorage.setItem("tareas", JSON.stringify(tareas));
-        mostrarDatos();
-    });
-});
-
-document.querySelectorAll(".btnCompleta").forEach((boton) => {
-    boton.addEventListener("click", (e) => {
-        tareas[e.target.dataset.id].estado = "pendiente";
-        localStorage.setItem("tareas", JSON.stringify(tareas));
-        mostrarDatos();
-    });
-});
-
-document.querySelectorAll(".btnBorrar").forEach((boton) => {
-    boton.addEventListener("click", (e) => {
-        tareas[e.target.dataset.id].estado = "eliminada";
-        localStorage.setItem("tareas", JSON.stringify(tareas));
-        mostrarDatos();
-    });
-});
-
-document.querySelectorAll(".btnEditar").forEach((boton) => {
-    boton.addEventListener("click", (e) => {
-        modificarTarea(e.target.dataset.id);
-    });
-});
 
 mostrarDatos();
 
-function modificarTarea(id){
-    document.querySelector("#cortina").computedStyleMap.display = "flex";
+function modificarTarea(id) {
+    document.querySelector("#cortina").style.display = "flex";
     document.querySelector("#modificarID").value = tareas[id].id;
     document.querySelector("#modificarTarea").value = tareas[id].tarea;
     document.querySelector("#modificarEstado").value = tareas[id].estado;
 };
 
 document.querySelector("#btnCancelarModifTarea").addEventListener("click", () => {
-    document.querySelector("#cortina").computedStyleMap.display = "none";
+    document.querySelector("#cortina").style.display = "none";
 });
 
-document.querySelector("#btnModifTarea").addEventListener("click", ()=> {
+document.querySelector("#btnModifTarea").addEventListener("click", () => {
     let id = document.querySelector("#modificarID").value;
-    tareas[id].tarea = document.querySelector("#modificarID").value;
+    tareas[id].tarea = document.querySelector("#modificarTarea").value;
     tareas[id].estado = document.querySelector("#modificarEstado").value;
     localStorage.setItem("tareas", JSON.stringify(tareas));
     mostrarDatos();
-    document.querySelector("#cortina").computedStyleMap.display = "none";
+    document.querySelector("#cortina").style.display = "none";
 });
